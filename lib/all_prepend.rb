c94eef8ea215e342833c88db284ba4f4dedbce8c
Nate Berkopec <nate.berkopec@gusto.com>
2022-02-25 11:07:29 -0700

    Prepend: All, add to CI

---
+++ lib/all_prepend.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'curb_prepend'
+require 'ethon_prepend'
+require 'eventmachine_prepend'
+require 'excon_prepend'
+require 'http_prepend'
+require 'httpclient_prepend'
+require 'net_http_prepend'
+require 'patron_prepend'
